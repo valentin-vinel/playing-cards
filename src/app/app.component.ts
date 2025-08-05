9c5be378257d7821a024dd9076a308eb28737f24
valentin-vinel <valentinvnl84@gmail.com>
2025-08-05 17:16:13 +0200

    :hammer: Signals output et models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/monster.model';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayingCardComponent],
+  imports: [PlayingCardComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   monster1!: Monster;
+  count: number = 0;
 
   constructor() {
     this.monster1 = new Monster();
     this.monster1.name = "Pik";
     this.monster1.hp = 40;
     this.monster1.figureCaption = "N°002 Pik";
+  }
+
+  increaseCount() {
+    this.count++;
   }
 
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { concatMapTo } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
 
+  @Input() search = 'Initial';
+
+  @Output() searchButtonClicked = new EventEmitter;
+
+  searchClick() {
+    this.searchButtonClicked.emit()
+  }
 }
