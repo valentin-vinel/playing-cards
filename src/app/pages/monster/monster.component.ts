1ea2e04c8634a28e84532c6940591198c25848e8
valentin-vinel <valentinvnl84@gmail.com>
2025-08-06 16:51:18 +0200

    Routes

--- src/app/pages/monster/monster.component.ts
+++ src/app/pages/monster/monster.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-monster',
@@ -8,17 +9,22 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './monster.component.html',
   styleUrl: './monster.component.css'
 })
-export class MonsterComponent implements OnInit {
+export class MonsterComponent implements OnInit, OnDestroy {
 
   private route = inject(ActivatedRoute);
   private router = inject(Router);
 
   monsterId = signal<number | undefined>(undefined);
+  routeSubsciption: Subscription | null = null;
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.routeSubsciption = this.route.params.subscribe(params => {
       this.monsterId.set(params['id'] ? parseInt(params['id']) : undefined);
     })
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubsciption?.unsubscribe();
   }
 
   next() {
